6ce770832a375df10e53470a77a79581ab80c01a
Leanne Northrop <leanne.northrop@googlemail.com>
2009-11-08 09:40:36 +0000

    Added datasource.

--- tools/deploy/config/deploy/stages/hudson.rb
+++ tools/deploy/config/deploy/stages/hudson.rb
@@ -3,3 +3,9 @@ role :app, '205.143.144.231'
 # This file is only intended to deploy so we use a different url here.
 set :repository, 'http://205.143.144.244:8080/job/MainBuild/lastSuccessfulBuild/artifact/war/target'
 set :artifact_id, 'london-samye-dzong'
+set :tomcat_ds, {
+  :username => 'lsd',
+  :password => 'password',
+  :host => '205.143.144.231',
+  :port => 5432
+}
