64a6f0753a5b0fa89c1b6fe33796bb177ea21148
Leanne Northrop <leanne.northrop@googlemail.com>
2009-11-08 13:57:42 +0000

    Moved database driver to tomcat install. Normalize permissions on tomcat directories to deploy setup task. Note dirs should probably belong to root but it can be changed later.

--- tools/deploy/config/deploy/recipes/deploy.rb
+++ tools/deploy/config/deploy/recipes/deploy.rb
@@ -8,6 +8,10 @@ namespace :deploy do
     database.install
     database.start
     sudo "yum clean all -q -y"
+    permissions.normalise "/opt/apache-tomcat-6.0.20", :owner => application, :group => 'tomcat'
+    permissions.normalise "/opt/tomcat", :owner => application, :group => 'tomcat'
+    sudo "chmod +x /opt/tomcat/bin/*.sh"
+    puts "You may need to add lsd to the tomcat group and the sudoers file, also check if ports are open"
   end
 
   after 'deploy:teardown' do
--- tools/deploy/config/deploy/recipes/tomcat.rb
+++ tools/deploy/config/deploy/recipes/tomcat.rb
@@ -1,10 +1,6 @@
 namespace :tomcat do
   after 'tomcat:install' do
-    sudo "#{wget} -nv http://mirrors.ibiblio.org/pub/mirrors/maven2/postgresql/postgresql/8.4-701.jdbc4/postgresql-8.4-701.jdbc4.jar -P '/opt/tomcat/lib'}"
-    permissions.normalise "/opt/apache-tomcat-6.0.20", :owner => application, :group => 'tomcat'
-    permissions.normalise "/opt/tomcat", :owner => application, :group => 'tomcat'
-    sudo "chmod +x /opt/tomcat/bin/*.sh"
-    puts "You may need to add lsd to the tomcat group and the sudoers file, also check if ports are open"
+    sudo "#{wget} -nv http://mirrors.ibiblio.org/pub/mirrors/maven2/postgresql/postgresql/8.4-701.jdbc4/postgresql-8.4-701.jdbc4.jar -P '/opt/tomcat/lib'"
   end
 
 after 'tomcat:uninstall' do
