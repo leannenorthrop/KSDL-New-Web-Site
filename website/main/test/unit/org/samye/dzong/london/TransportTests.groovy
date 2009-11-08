0c1533bc9bbd246a0c11344782a0527f596a8d05
leanne.northrop <leanne.northrop@256981f4-bd85-11de-ac70-2fe2a02b5c02>
2009-11-08 21:37:35 +0000

    Playing with Grails. Changed grails build to use the resources from trunk/Graphics.
    git-svn-id: https://lsd-website.googlecode.com/svn/trunk@40 256981f4-bd85-11de-ac70-2fe2a02b5c02

---
+++ website/main/test/unit/org/samye/dzong/london/TransportTests.groovy
@@ -0,0 +1,17 @@
+package org.samye.dzong.london
+
+import grails.test.*
+
+class TransportTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ website/main/test/unit/org/samye/dzong/london/event/RegularEventTests.groovy
@@ -0,0 +1,17 @@
+package org.samye.dzong.london.event
+
+import grails.test.*
+
+class RegularEventTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
