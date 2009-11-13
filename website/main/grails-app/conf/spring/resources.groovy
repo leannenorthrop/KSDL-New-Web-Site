60b98254c1953b8526929e4690c63165bcbacbf1
Leanne Northrop <leanne.northrop@googlemail.com>
2009-11-13 13:21:44 +0000

    Implemented log-in and log out.

--- website/main/grails-app/conf/spring/resources.groovy
+++ website/main/grails-app/conf/spring/resources.groovy
@@ -1,4 +1,6 @@
-// Place your Spring DSL code here
+import org.apache.shiro.authc.credential.Sha512CredentialsMatcher
 beans = {
-    
+    //credentialMatcher(Sha512CredentialsMatcher) {
+    //    storedCredentialsHexEncoded = true
+    //}
 }
