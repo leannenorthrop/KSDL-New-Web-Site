809e963a3254cf52ab7b8b2878479af770059f47
Leanne Northrop <leanne.northrop@googlemail.com>
2009-11-13 13:23:02 +0000

    Added profile and article.

---
+++ website/main/grails-app/domain/org/samye/dzong/london/community/Article.groovy
@@ -0,0 +1,14 @@
+package org.samye.dzong.london.community
+import org.samye.dzong.london.User
+
+class Article {
+    String title;
+    String summary;
+    String content;
+
+    static constraints = {
+        title(blank:false)
+        summary(maxSize:1024)
+        content(maxSize:Integer.MAX_VALUE)
+    }
+}
