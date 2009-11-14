5e00c313d46b1db37f214af58d0217af3090aac9
leanne.northrop <leanne.northrop@256981f4-bd85-11de-ac70-2fe2a02b5c02>
2009-11-14 15:10:41 +0000

    Changed database to hsqld for integration until SQL fixes can be found,
    git-svn-id: https://lsd-website.googlecode.com/svn/trunk@60 256981f4-bd85-11de-ac70-2fe2a02b5c02

--- tools/deploy/config/deploy.rb
+++ tools/deploy/config/deploy.rb
@@ -3,8 +3,8 @@ set :group_id, 'org.northrop.leanne'
 set :artifact_id, 'lsd'
 set :version, '1.0'
 set :packaging, 'war'
-set :tomcat_java_opts, '-Xms512m -Xmx812m'
+set :tomcat_java_opts, '-Xms256m -Xmx812m -XX:PermSize=256m'
 set :http_get_method, 'wget -nv'
 set :apache_path, 'apache'
-ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "lsd.ppk")]
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "lsdci.ppk")]
 
