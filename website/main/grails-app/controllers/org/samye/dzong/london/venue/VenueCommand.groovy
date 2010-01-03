4a64ea853f0b350752673ee7bf0a78a79bb4f8a3
leanne.northrop <leanne.northrop@256981f4-bd85-11de-ac70-2fe2a02b5c02>
2010-01-03 22:50:21 +0000

    Added room sub command to try to create venue rooms.
    git-svn-id: https://lsd-website.googlecode.com/svn/trunk@127 256981f4-bd85-11de-ac70-2fe2a02b5c02

--- website/main/grails-app/controllers/org/samye/dzong/london/venue/VenueCommand.groovy
+++ website/main/grails-app/controllers/org/samye/dzong/london/venue/VenueCommand.groovy
@@ -1,12 +1,15 @@
 package org.samye.dzong.london.venue
 
+import org.apache.commons.collections.ListUtils
+import org.apache.commons.collections.FactoryUtils
+    
 class VenueCommand { 
     String name;
     String imageName;
     String description;
     String facilities;
     String access;  
-    List rooms = [];
+    List<RoomCommand> rooms = ListUtils.lazyList([], FactoryUtils.instantiateFactory(RoomCommand))
     
     static constraints = {
     	name(size:5..512)
@@ -21,14 +24,31 @@ class VenueCommand {
         def venue = new Venue(name:name, description:description, facilities:facilities, access:access);
         def image = Image.findByName(imageName)
         venue.image = Image
-        rooms.eachWithIndex { roomName, roomImageName, roomDescription, roomMakePublic, i -> 
-            def room = new Room(name: roomName, description: description)
-            def roomImage = Image.findByName(roomImageName)
-            if (roomImage) { room.image = roomImage }
-            room.publishState = roomMakePublic ? "Published" : "Unpublished";
-            room.deleted = false;
-            venue.addToRooms(room) 
+        rooms.eachWithIndex { room, i ->             
+            venue.addToRooms(room.createRoom()) 
         } 
         return venue
+    }
+}
+
+class RoomCommand {
+    String name
+    String description
+    String imageName
+    Boolean makePublic
+
+    static constraints = {
+        name(nullable: false, blank: false)
+        imageName(nullable: false, blank: false)        
+        description(nullable: true, blank: true)
+    }
+    
+    Room createRoom() {
+        def room = new Room(name:name, description:description)
+        def roomImage = Image.findByName(imageName)
+        if (roomImage) { room.image = roomImage }
+        room.publishState = roomMakePublic ? "Published" : "Unpublished";
+        room.deleted = false; 
+        return room;       
     }
 }
