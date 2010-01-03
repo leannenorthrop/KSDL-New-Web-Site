d42bd8d7db4f5174e359acdccb0d85aec8ba628c
Leanne Northrop <leanne.northrop@googlemail.com>
2010-01-03 22:47:40 +0000

    Moved command object to own file.

--- website/main/grails-app/controllers/org/samye/dzong/london/venue/VenueController.groovy
+++ website/main/grails-app/controllers/org/samye/dzong/london/venue/VenueController.groovy
@@ -1,42 +1,6 @@
-
-
 package org.samye.dzong.london.venue
 
-class VenueController {
-
-    class VenueCommand { 
-        String name;
-        String imageName;
-        String description;
-        String facilities;
-        String access;  
-        List rooms = [];
-        
-        static constraints = {
-        	name(size:5..512)
-        	image(nullable:true)
-        	description(size:5..32000)
-        	facilities(blank:true)
-        	access(blank:true)
-        	rooms(nullable:true)
-        }          
-        
-        Venue createVenue() {
-            def venue = new Venue(name:name, description:description, facilities:facilities, access:access);
-            def image = Image.findByName(imageName)
-            venue.image = Image
-            rooms.eachWithIndex { roomName, roomImageName, roomDescription, roomMakePublic, i -> 
-                def room = new Room(name: roomName, description: description)
-                def roomImage = Image.findByName(roomImageName)
-                if (roomImage) { room.image = roomImage }
-                room.publishState = roomMakePublic ? "Published" : "Unpublished";
-                room.deleted = false;
-                venue.addToRooms(room) 
-            } 
-            return venue
-        }
-    }
-    
+class VenueController {    
     // the delete, save and update actions only accept POST requests
     static allowedMethods = [delete:'POST', save:'POST', update:'POST']
 
