d9456775d60f40cd6f221c36144719c0dd41af0d
leanne.northrop <leanne.northrop@256981f4-bd85-11de-ac70-2fe2a02b5c02>
2010-01-28 23:05:27 +0000

    Added theme switching page. Updated footer with sections with blank pages for each.
    git-svn-id: https://lsd-website.googlecode.com/svn/trunk@145 256981f4-bd85-11de-ac70-2fe2a02b5c02

--- website/main/grails-app/taglib/org/samye/dzong/london/CssThemesTagLib.groovy
+++ website/main/grails-app/taglib/org/samye/dzong/london/CssThemesTagLib.groovy
@@ -2,17 +2,27 @@ package org.samye.dzong.london
 import java.io.File
 import static org.apache.commons.lang.WordUtils.*
 import org.apache.shiro.SecurityUtils
+import javax.servlet.http.Cookie
 
 class CssThemesTagLib {
     static namespace = 'lsd'
 
-    def link = { attrs ->
-        def application = attrs.obj
+    def cssTheme = { attrs ->
+        def themeCookie = request.getCookies().find() { it.name == "cssTheme"}
+        if (!themeCookie) {
+            themeCookie = new Cookie("cssTheme","default")
+            themeCookie.setMaxAge(60*60*24*365)
+            themeCookie.setPath("/")
+            response.addCookie(themeCookie)
+        }
+        def application = attrs.app
         new File(application.getRealPath("/css/themes")).listFiles().each{ file ->
             if (file.isDirectory()) {
                 def name = file.name
-                def href = resource(dir: "css/themes/${name}", file:'screen.css')
-                out << "<link rel=\"alternate stylesheet\" type=\"text/css\" media=\"screen, projection\" href=\"${href}\" title=\"${capitalize(name)}\" />\n"
+                if (name == themeCookie.value) {
+                    def href = resource(dir: "css/themes/${name}", file:'screen.css')
+                    out << "<link rel=\"stylesheet\" type=\"text/css\" media=\"screen, projection\" href=\"${href}\" title=\"${capitalize(name)}\" />\n"
+                }
             }
         }
     }
---
+++ website/main/grails-app/views/home/aboutThisSite.gsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>About This Site</title>
+    <meta name="layout" content="main">
+</head>
+<body>
+        About This Web Site
+</body>
+</html>
