46a84b63e472d70ccaea4fc04e4ea3951edcd13a
leanne.northrop <leanne.northrop@256981f4-bd85-11de-ac70-2fe2a02b5c02>
2010-05-09 17:33:25 +0000

    Changed config for production
    git-svn-id: https://lsd-website.googlecode.com/svn/trunk@219 256981f4-bd85-11de-ac70-2fe2a02b5c02

--- website/main/grails-app/conf/Config.groovy
+++ website/main/grails-app/conf/Config.groovy
@@ -31,8 +31,8 @@ grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
 grails.converters.encoding="UTF-8"
 // enable Sitemesh preprocessing of GSP pages
-grails.views.gsp.sitemesh.preprocess=false
-grails.gsp.enable.reload = true
+grails.views.gsp.sitemesh.preprocess=true
+grails.gsp.enable.reload = false
 // scaffolding templates configuration
 grails.scaffolding.templates.domainSuffix = 'Instance'
 
